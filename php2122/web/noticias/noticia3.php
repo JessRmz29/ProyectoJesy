74287434f270dba3fb90161a361e3aca6a76b584
Jessica Ramirez Carmona <jess_290793@hotmail.com>
2016-05-02 15:05:47 -0600

    Plantilla PHP

---
+++ php2122/web/noticias/noticia3.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<table width="260" border="0" cellpadding="4" cellspacing="0">
+  <tr>
+    <td><p align="center"><img src="noticias/fotos/foto3.jpg" alt="titular de la noticia3" width="231" height="158" hspace="4" vspace="0" border="0"></p>
+      <p>No depender&aacute; de terceros para 
+          actualizar precios, caracter&iacute;sticas o cambiar 
+        las fotograf&iacute;as de los productos, y s&oacute;lo con conocimientos a nivel de usuario. </p>
+      <p>Podemos adaptar las cestas de la compra a cualquier necesidad particular que usted tenga como, a&ntilde;adir opciones, descuentos, impuestos, etc. </p>
+      <p><a href="http://www.brcmedia.com/catalogo/tiendas/tienda.php" class="brcenlaces" title="Todas las tiendas virtuales de Brcmedia.com. Para vender sus productos por Internet desde ya. Tiendas on-line muy economicas. Tiendas virtuales baratas y f&aacute;ciles de actualizar">En el apartado de nuestro cat&aacute;logo dedicado a tiendas online puede ver todas las p&aacute;ginas web que incluyen galer&iacute;as de productos, cesta de la compra, formulario de pedidos, ...</a></p>
+        <p><strong>Conexi&oacute;n de la TPV virtual:</strong><br>
+  Si no tiene conocimientos para conectar la p&aacute;gina web con la TPV de &quot;la Caixa&quot;en Brcmedia le podemos realizar esa conexi&oacute;n. <br>
+  <a href="http://www.brcmedia.com/empresa/presupuestos.html" class="brcenlaces" rel="nofollow" title="Solicitar presupuesto">Solicite presupuesto aqu&iacute;.</a><br>
+    (S&oacute;lo para conexiones con &quot;la Cai<font color="#4C4639">xa&quot;)</font></p>    </td>
+  </tr>
+</table>
